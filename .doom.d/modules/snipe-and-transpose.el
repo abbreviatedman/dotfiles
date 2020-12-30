a0601ec6e2391405eee8a520f3310e2fce182faa
Colin Jaffe <balloonasaurus@gmail.com>
2020-12-30 06:57:30 -0500

    Removes highlighting of match after snipe-and-transpose actions.

--- .doom.d/modules/snipe-and-transpose.el
+++ .doom.d/modules/snipe-and-transpose.el
@@ -5,7 +5,8 @@
     (evil-snipe-S 1 chars)
     (evil-forward-char)
     (transpose-chars 1)
-    (evil-backward-char 2)))
+    (evil-backward-char 2))
+  (remove-overlays))
 
 (defun evil-snipe-and-transpose (chars)
   (interactive "M")
@@ -13,7 +14,8 @@
     (evil-snipe-s 1 chars)
     (evil-forward-char)
     (transpose-chars 1)
-    (evil-backward-char 2)))
+    (evil-backward-char 2))
+  (remove-overlays))
 
 ;; These mappings are backwards of the usual "lowercase goes forward, uppercase goes backwards" vim standards, but I almost always want to transpose backwards, having made a mistake and moved on in the text. You could always change them back!
 (map! :map evil-normal-state-map "SPC r" #'evil-snipe-back-and-transpose)
