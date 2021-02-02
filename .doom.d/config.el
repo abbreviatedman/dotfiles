d2d36905cdb8a1b4d6d81c78b5be136afe904f90
Colin Jaffe <balloonasaurus@gmail.com>
2021-02-02 06:11:02 -0500

    Adds TODO for fixing completion-menu number-based keybinding.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -251,6 +251,13 @@
 (global-set-key (kbd "M-p") #'counsel-yank-pop)
 
 ;; Adds the ability to type Meta-n to go to the nth item in an Ivy buffer.
+;; Not working on startup, but CAN be run after startup when the `ivy-minibuffer-map`
+;; variable exists.
+;;
+;; Could map setting it up with a keypress when I discover the feature doesn't exist.
+;; But there's probably a workaround where we can have it activate later on startup.
+;;
+;; When a file is first opened? We probably want to access it earlier.
 ;; (defun ivy-call-number (n)
 ;;   (interactive
 ;;    (list (let* ((type (event-basic-type last-command-event))
