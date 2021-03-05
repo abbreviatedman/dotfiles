9fb38344965ef83972487bcf8a57ad326ee4c840
Colin Jaffe <balloonasaurus@gmail.com>
2021-03-05 08:24:01 -0500

    Adds file previews in dired.

--- .doom.d/modules/config-dired.el
+++ .doom.d/modules/config-dired.el
@@ -2,5 +2,6 @@
 (defun setup-dired () (dired-hide-details-mode 1))
 (add-hook 'dired-mode-hook #'setup-dired)
 (map! :map :n :leader "d" #'dired-jump)
-(evil-define-key 'normal dired-mode-map "T" nil)
 (evil-define-key 'normal dired-mode-map "T" 'dired-create-empty-file)
+(defun toggle-peep-dired () (interactive) (peep-dired))
+(evil-define-key 'normal dired-mode-map (kbd "C-;") 'toggle-peep-dired)
