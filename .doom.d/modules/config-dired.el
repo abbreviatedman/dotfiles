84f8408ae3092a7aff437d170a5f476acf1e7e9a
Colin Jaffe <balloonasaurus@gmail.com>
2021-03-05 08:41:55 -0500

    Simplifies dired hook.

--- .doom.d/modules/config-dired.el
+++ .doom.d/modules/config-dired.el
@@ -1,6 +1,5 @@
 ;;; Dired
-(defun setup-dired () (dired-hide-details-mode 1))
-(add-hook 'dired-mode-hook #'setup-dired)
+(add-hook 'dired-mode-hook #'dired-hide-details-mode)
 (map! :map :n :leader "d" #'dired-jump)
 (evil-define-key 'normal dired-mode-map "T" 'dired-create-empty-file)
 (defun toggle-peep-dired () (interactive) (peep-dired))
