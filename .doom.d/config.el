0a494012c318329c0109f77458cfaaa87abb9b0d
Colin Jaffe <balloonasaurus@gmail.com>
2021-03-29 06:55:09 -0400

    Adds in-progress ivy-hydra-fork.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -8,6 +8,8 @@
 (load-library "config-dired")
 (load-library "terminals")
 (load-library "aliases")
+;; currently not overwriting
+(load-library "ivy-hydra-fork")
 
 ;; TODO mark checkboxes marked with "SPC m x"
 ;; TODO make returning to transparency not additive
