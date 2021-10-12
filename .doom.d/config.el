e8e29bd4b0f4a91dd4d128807c3f6f8b1ad43c21
Colin Jaffe <balloonasaurus@gmail.com>
2021-10-12 15:19:02 -0400

    Adds Emacs Everywhere.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -459,6 +459,8 @@
 ;; No fringe
 (fringe-mode 0)
 
+;; Simpler binding for Emacs Everywhere
+(map! :map emacs-everywhere-mode-map :ni (kbd "C-c ") #'emacs-everywhere-finish-or-ctrl-c-ctrl-c)
 
 ; some available keybinding prefixes
 ;; SPC l
