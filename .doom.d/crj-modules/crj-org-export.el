dfdedab54a255c80bd5f58669869eed9e8129183
Colin Jaffe <balloonasaurus@gmail.com>
2022-07-31 16:55:00 -0700

    Adds prettifying on org src blocks.

--- .doom.d/crj-modules/crj-org-export.el
+++ .doom.d/crj-modules/crj-org-export.el
@@ -11,6 +11,20 @@
                                         (:session . "none")
                                         (:eval . "no-export"))))
 
+(defun crj/prettify-js-org-src-block ()
+  "Run prettier on source block at point."
+  (interactive)
+  (org-edit-src-code)
+  (prettier-prettify)
+  (org-edit-src-exit))
+
+(map! :map org-mode-map
+      :leader
+      (:prefix "m"
+       (:prefix "p"
+        :desc "Prettify source block at point."
+        :n "j" #'crj/prettify-js-org-src-block)))
+
 ;; Quick and dirty CSS.
 ;; TODO add to it, maybe make it a global file or host on a CDN.
 (setq org-html-head-extra
