0b8e49b47039987ce9c94f0dbc337bc46e2824ce
Colin Jaffe <balloonasaurus@gmail.com>
2022-08-19 22:05:05 -0400

    Add keybinding for reading the Elisp book.

--- .doom.d/crj-modules/crj-elisp.el
+++ .doom.d/crj-modules/crj-elisp.el
@@ -1,3 +1,15 @@
+; Learning elisp.
+
+(defun crj/open-intro-to-elisp ()
+  "Open the manual for \"An Introduction to Programming in Emacs Lisp\" (Info node '(eintr)'), re-using the current window."
+  (interactive)
+  (info (Info-find-file "eintr"))
+  (delete-other-windows))
+
+(map! :leader (:prefix "o"
+               :n "l" #'crj/open-intro-to-elisp))
+
+;; Evaluate things in Info Mode.
 (map! :map Info-mode-map
       :n "C-c C-e" #'eros-eval-last-sexp
       :n "C-c e" #'eros-eval-last-sexp)
