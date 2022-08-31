60443eaf40ebdc2cf75060df3f849ade7a7a9485
Colin Jaffe <balloonasaurus@gmail.com>
2022-08-31 00:33:14 -0400

    Fixes mail settings.

--- .doom.d/crj-modules/crj-mail.el
+++ .doom.d/crj-modules/crj-mail.el
@@ -1,53 +1,51 @@
-;; (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
-;; (require 'mu4e)
-;; (require 'mu4e-contrib)
-;; (require 'smtpmail)
-;; (require 'org-mime)
-
-(setq user-full-name  "Colin Jaffe"
-      mu4e-attachment-dir "~/Downloads"
-      user-mail-address "colin@pursuit.org"
-      smtpmail-default-smtp-server "Posteo.de"
-      smtpmail-smtp-server "posteo.de"
-      smtpmail-smtp-service 587
-      ;; mu4e-maildir-shortcuts
-      ;; '(("/INBOX"      . ?i)
-      ;;   ("/am-posteo/[am].Sent" . ?s)
-      ;;   ("/am-posteo/[am].Drafts"     . ?d)
-      ;; ("/am-posteo/[am].Trash"      . ?t))
-      smtpmail-stream-type 'starttls
-      )
-
-(set-email-account!
- "posteo.net"
- '((mu4e-sent-folder       . "/am-posteo/[am].Sent")
-   (mu4e-drafts-folder     . "/am-posteo/Drafts")
-   (mu4e-trash-folder      . "/am-posteo/[am].Trash")
-   (mu4e-refile-folder     . "/am-posteo/INBOX")
-   (smtpmail-smtp-user     . "abbreviatedman@posteo.net")
-   (+mu4e-personal-addresses . '("colin@pursuit.org"
-                                 "colin.jaffe@gmail.com"
-                                 "balloonasaurus@gmail.com"
-                                 "abbreviatedman@posteo.net"
-                                 "abbreviatedman@posteo.af")))
- t)
+(after! mu4e
+  (mu4e-alert-set-default-style 'message)
+  (setq user-mail-address "abbreviatedman@posteo.net"
+        doom-modeline-mu4e nil
+        +mu4e-alert-bell-cmd nil
+        sendmail-program (executable-find "msmtp")
+        send-mail-function #'smtpmail-send-it
+        message-sendmail-f-is-evil t
+        message-sendmail-extra-arguments '("--read-envelope-from")
+        message-send-mail-function #'message-send-mail-with-sendmail
+        user-full-name  "Colin Jaffe"
+        mu4e-get-mail-command "mbsync --all -c ~/.mbsyncrc"
+        mu4e-update-interval nil
+        mu4e-main-hide-personal-addresses t
+        mu4e-drafts-folder "/Drafts"
+        mu4e-trash-folder "/Trash"
+        mu4e-sent-folder "/Sent"
+        mu4e-index-cleanup t
+        mu4e-index-lazy-check nil
+        mu4e-attachment-dir "~/Downloads"
+        smtpmail-debug-info t
+        +mu4e-personal-addresses '("colin@pursuit.org"
+                                   "colin.jaffe@gmail.com"
+                                   "balloonasaurus@gmail.com"
+                                   "abbreviatedman@posteo.net")
+        mu4e-maildir-shortcuts '(("/INBOX"      . ?i)
+                                 ("/Sent" . ?s)
+                                 ("/Drafts"     . ?d)
+                                 ("/Trash"      . ?t))))
 
 
-(require 'org-msg)
+(defun crj/mark-all-as-read ()
+  "Marks all visible messages as read.
 
-(defun crj/send-mail ()
+Also updates the server to match."
   (interactive)
-  (setq smtpmail-smtp-server "posteo.de")
-  (org-ctrl-c-ctrl-c))
+  (mu4e-headers-mark-for-each-if
+   '(read . nil)
+   (lambda (msg _param) t))
+  (mu4e-mark-execute-all t)
+  (mu4e-update-mail-and-index t))
 
-;; (setq org-msg-default-alternatives '(html))
-;; (map! :map 'org-msg-edit-mode-map :ni "C-c C-c" #'crj/send-mail)
+(map! :map mu4e-headers-mode-map :n "M" 'crj/mark-all-as-read)
 
-
-;; Forget where I got this. It wasn't mine!
-;; (But I wasn't always good about crediting sources.)
 (defun crj/set-from-address ()
-  "Set the From address based on the To address of the original."
+  "Set the From address based on the To address of the original.
+
+Forget where I got this, but it's pretty sweet."
   (let ((msg mu4e-compose-parent-message))
     (when msg
       (setq user-mail-address
