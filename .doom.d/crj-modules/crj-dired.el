ac6777351a28df725bc23129930a35b5c5858393
Colin Jaffe <balloonasaurus@gmail.com>
2022-09-06 15:01:44 -0400

    Add basic dired setup.

--- .doom.d/crj-modules/crj-dired.el
+++ .doom.d/crj-modules/crj-dired.el
@@ -1,1 +1,9 @@
 (map! :leader :n "d" #'dired-jump)
+
+(defun crj-set-up-dired ()
+  (dired-omit-mode)
+  (dired-hide-details-mode))
+
+(use-package! dired-x
+  :config
+  (add-hook 'dired-mode-hook #'crj-set-up-dired))
