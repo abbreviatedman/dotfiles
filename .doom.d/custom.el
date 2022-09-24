2173f68bacbf63c96c72bc27132712d6bc51c7b5
Colin Jaffe <balloonasaurus@gmail.com>
2022-09-23 20:42:53 -0400

    Adds custom file.

---
+++ .doom.d/custom.el
@@ -0,0 +1,92 @@
+(put 'customize-group 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["gray35" "#ff8059" "#44bc44" "#d0bc00" "#2fafff" "#feacd0" "#00d3d0" "gray65"])
+ '(auth-source-save-behavior nil)
+ '(awesome-tray-mode-line-active-color "#2fafff")
+ '(awesome-tray-mode-line-inactive-color "#323232")
+ '(custom-safe-themes
+   '("d6da24347c813d1635a217d396cf1e3be26484fd4d05be153f3bd2b293d2a0b5" default))
+ '(exwm-floating-border-color "#646464")
+ '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-themes-fringe-red))
+ '(flymake-note-bitmap '(exclamation-mark modus-themes-fringe-cyan))
+ '(flymake-warning-bitmap '(exclamation-mark modus-themes-fringe-yellow))
+ '(highlight-tail-colors '(("#2f4a00" . 0) ("#00415e" . 20)))
+ '(hl-todo-keyword-faces
+   '(("HOLD" . "#c0c530")
+     ("TODO" . "#feacd0")
+     ("NEXT" . "#b6a0ff")
+     ("THEM" . "#f78fe7")
+     ("PROG" . "#00d3d0")
+     ("OKAY" . "#4ae2f0")
+     ("DONT" . "#70b900")
+     ("FAIL" . "#ff8059")
+     ("BUG" . "#ff8059")
+     ("DONE" . "#44bc44")
+     ("NOTE" . "#d3b55f")
+     ("KLUDGE" . "#d0bc00")
+     ("HACK" . "#d0bc00")
+     ("TEMP" . "#ffcccc")
+     ("FIXME" . "#ff9077")
+     ("XXX+" . "#ef8b50")
+     ("REVIEW" . "#6ae4b9")
+     ("DEPRECATED" . "#bfd9ff")))
+ '(ibuffer-deletion-face 'modus-themes-mark-del)
+ '(ibuffer-filter-group-name-face 'modus-themes-pseudo-header)
+ '(ibuffer-marked-face 'modus-themes-mark-sel)
+ '(ibuffer-title-face 'default)
+ '(jest-arguments '("--colors" "--watchAll"))
+ '(org-src-block-faces 'nil)
+ '(pdf-view-midnight-colors '("#ffffff" . "#100f10"))
+ '(smtpmail-smtp-server "posteo.net")
+ '(sql-connection-alist
+   '(("songs"
+      (sql-product 'postgres)
+      (sql-user "abbreviatedman")
+      (sql-database "songs_dev")
+      (sql-server ""))) t)
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   '((20 . "#ff8059")
+     (40 . "#feacd0")
+     (60 . "#f78fe7")
+     (80 . "#ef8b50")
+     (100 . "#d0bc00")
+     (120 . "#c0c530")
+     (140 . "#f8dec0")
+     (160 . "#bfebe0")
+     (180 . "#44bc44")
+     (200 . "#70b900")
+     (220 . "#6ae4b9")
+     (240 . "#4ae2f0")
+     (260 . "#00d3d0")
+     (280 . "#c6eaff")
+     (300 . "#2fafff")
+     (320 . "#79a8ff")
+     (340 . "#00bcff")
+     (360 . "#b6a0ff")))
+ '(vc-annotate-very-old-color nil)
+ '(warning-suppress-types '((auto-save)))
+ '(xterm-color-names
+   ["black" "#ff8059" "#44bc44" "#d0bc00" "#2fafff" "#feacd0" "#00d3d0" "gray65"])
+ '(xterm-color-names-bright
+   ["gray35" "#ef8b50" "#70b900" "#c0c530" "#79a8ff" "#f78fe7" "#4ae2f0" "white"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight ((t :background "#b5d0ff")))
+ '(line-number ((t :family "Hack")))
+ '(line-number-current-line ((t :family "Hack")))
+ '(mode-line ((t (:family "Fira Code" :height 0.7))))
+ '(mode-line-highlight ((t :foreground "#d7d7d7" :background "#0030b4")))
+ '(mode-line-inactive ((t (:family "Fira Code" :height 0.7))))
+ '(success ((t :foreground "#0031a9"))))
