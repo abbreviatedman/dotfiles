dea0f44645583540348c1331aff1df12a279667e
Colin Jaffe <balloonasaurus@gmail.com>
2022-10-30 12:26:31 -0400

    Adds inactive gcal module.

---
+++ .doom.d/crj-modules/crj-org-gcal.el
@@ -0,0 +1,10 @@
+;;; crj-modules/crj-org-gcal.el -*- lexical-binding: t; -*-
+
+;; (defun org-gcal--get-access-token ()
+;;   (oauth2-auto-access-token-sync "colin@pursuit.org" 'google '(calendar)))
+
+;; (use-package! org-gcal
+;;   :init
+;;   (setq org-gcal-client-id "828280942583-h704b1o5cd1kq3p2bolsegg4mel5g7tb.apps.googleusercontent.com"
+;;         org-gcal-client-secret "fQG8VfDmzyuL7XRJ6nuf1T4f"
+;;         org-gcal-fetch-file-alist '(("colin@pursuit.org" . "~/org-stuff/gcal/colin@pursuit.org.org"))))
