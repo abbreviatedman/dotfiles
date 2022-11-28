cd948d55332916509cb08556bd8d979de7e621a0
Colin Jaffe <balloonasaurus@gmail.com>
2022-11-28 12:30:59 -0500

    Refactors and fixes keybinding bugs.

--- .doom.d/crj-modules/crj-markdown.el
+++ .doom.d/crj-modules/crj-markdown.el
@@ -6,29 +6,39 @@
 
 (use-package! markdown-mode
   :config
- (define-key markdown-mode-command-map (kbd "d") #'crj/kill-all-text-and-save)
- (add-hook 'markdown-mode-hook #'variable-pitch-mode)
-  (map!
-   :map (markdown-mode-map evil-markdown-mode-map)
-   :n "gj" #'evil-next-visual-line
-   :n "gk" #'evil-previous-visual-line
-   :i "TAB" #'markdown-demote
-   :i "<backtab>" #'markdown-promote
-   :i "M-b" nil)
+  (setq markdown-fontify-code-blocks-natively t
+        markdown-list-indent-width 2
+        markdown-indent-on-enter 'indent-and-new-item
+        markdown-header-scaling t
+        markdown-header-scaling-values '(2.5
+                                         2.0
+                                         2.0
+                                         1.5
+                                         1.0
+                                         1.0))
 
-  (setq markdown-header-scaling t)
-  (setq markdown-header-scaling-values '(2.5 2.0 2.0 1.5 1.0 1.0))
+  (define-key markdown-mode-command-map (kbd "d") #'crj/kill-all-text-and-save)
+  (add-hook 'markdown-mode-hook #'variable-pitch-mode)
+  (evil-define-key
+    'normal markdown-mode-map
+    "gj" #'evil-next-visual-line
+    "gk" #'evil-previous-visual-line)
+  (evil-define-key 'insert markdown-mode-map
+    (kbd "TAB") #'markdown-demote
+    (kbd "<backtab>") #'markdown-promote
+    (kbd "M-b") #'backward-word)
+  (define-key markdown-mode-style-map (kbd "b") #'backward-word)
 
   (map!
-   :map markdown-mode-map
    (:prefix "["
     :n "[" #'markdown-backward-same-level)
    (:prefix "]"
-    :n "]" #'markdown-forward-same-level))
+    :n "]" #'markdown-forward-same-level)))
 
-  ;; Tell markdown mode to stop over-indenting lists.
-  (setq markdown-list-indent-width 2)
-  ;; Make markdown continue lists on enter.
-  (setq markdown-indent-on-enter 'indent-and-new-item)
-  ;; (slow) syntax coloration in markdown blocks
-  (setq markdown-fontify-code-blocks-natively t))
+(use-package! evil-markdown
+  :after markdown-mode
+  :config
+  (evil-define-key 'insert evil-markdown-mode-map (kbd "M-b") nil)
+  (evil-define-key 'normal evil-markdown-mode-map
+    "gj" #'evil-next-visual-line
+    "gk" #'evil-previous-visual-line))
