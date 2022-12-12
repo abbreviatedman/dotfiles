9d710da01ffd4912cfa181d0d1eadc823454e485
Colin Jaffe <balloonasaurus@gmail.com>
2022-12-12 10:58:47 -0500

    Begins work on agenda customization.

--- .doom.d/crj-modules/crj-org.el
+++ .doom.d/crj-modules/crj-org.el
@@ -1,5 +1,100 @@
+(setq org-agenda-custom-commands '(("A" "Daily agenda and top priority tasks"
+  ((tags-todo "*"
+              ((org-agenda-skip-function
+                '(org-agenda-skip-if nil
+                  '(timestamp)))
+               (org-agenda-skip-function
+                `(org-agenda-skip-entry-if 'notregexp ,(format "\\[#%s\\]"
+                                                               (char-to-string org-priority-highest))))
+               (org-agenda-block-separator nil)
+               (org-agenda-overriding-header "Important tasks without a date\n")))
+   (agenda ""
+           ((org-agenda-span 1)
+            (org-deadline-warning-days 0)
+            (org-agenda-block-separator nil)
+            (org-scheduled-past-days 0)
+            (org-agenda-day-face-function
+             (lambda
+               (date)
+               'org-agenda-date))
+            (org-agenda-format-date "%A %-e %B %Y")
+            (org-agenda-overriding-header "\nToday's agenda\n")))
+   (agenda ""
+           ((org-agenda-start-on-weekday nil)
+            (org-agenda-start-day "+1d")
+            (org-agenda-span 7)
+            (org-deadline-warning-days 0)
+            (org-agenda-block-separator nil)
+            (org-agenda-skip-function
+             '(org-agenda-skip-entry-if 'todo 'done))
+            (org-agenda-overriding-header "\nNext Seven Days\n")))
+   (agenda ""
+           ((org-agenda-time-grid nil)
+            (org-agenda-start-on-weekday nil)
+            (org-agenda-start-day "+4d")
+            (org-agenda-span 14)
+            (org-agenda-show-all-dates nil)
+            (org-deadline-warning-days 0)
+            (org-agenda-block-separator nil)
+            (calendar-mode)
+            (org-agenda-entry-types
+             '(:deadline))
+            (org-agenda-skip-function
+             '(org-agenda-skip-entry-if 'todo 'done))
+            (org-agenda-overriding-header "\nUpcoming deadlines (+14d)\n")))))
+ ("P" "Plain text daily agenda and top priorities"
+  ((tags-todo "*"
+              ((org-agenda-skip-function
+                '(org-agenda-skip-if nil
+                  '(timestamp)))
+               (org-agenda-skip-function
+                `(org-agenda-skip-entry-if 'notregexp ,(format "\\[#%s\\]"
+                                                               (char-to-string org-priority-highest))))
+               (org-agenda-block-separator nil)
+               (org-agenda-overriding-header "Important tasks without a date\n")))
+   (agenda ""
+           ((org-agenda-span 1)
+            (org-deadline-warning-days 0)
+            (org-agenda-block-separator nil)
+            (org-scheduled-past-days 0)
+            (org-agenda-day-face-function
+             (lambda
+               (date)
+               'org-agenda-date))
+            (org-agenda-format-date "%A %-e %B %Y")
+            (org-agenda-overriding-header "\nToday's agenda\n")))
+   (agenda ""
+           ((org-agenda-start-on-weekday nil)
+            (org-agenda-start-day "+1d")
+            (org-agenda-span 3)
+            (org-deadline-warning-days 0)
+            (org-agenda-block-separator nil)
+            (org-agenda-skip-function
+             '(org-agenda-skip-entry-if 'todo 'done))
+            (org-agenda-overriding-header "\nNext three days\n")))
+   (agenda ""
+           ((org-agenda-time-grid nil)
+            (org-agenda-start-on-weekday nil)
+            (org-agenda-start-day "+4d")
+            (org-agenda-span 14)
+            (org-agenda-show-all-dates nil)
+            (org-deadline-warning-days 0)
+            (org-agenda-block-separator nil)
+            (org-agenda-entry-types
+             '(:deadline))
+            (org-agenda-skip-function
+             '(org-agenda-skip-entry-if 'todo 'done))
+            (org-agenda-overriding-header "\nUpcoming deadlines (+14d)\n"))))
+  ((org-agenda-with-colors nil)
+   (org-agenda-prefix-format "%t %s")
+   (org-agenda-current-time-string "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+   (org-agenda-fontify-priorities nil)
+   (org-agenda-remove-tags t))
+  ("agenda.txt"))))
+
 ;; Stop indenting my code blocks. Seriously!
 (setq org-edit-src-content-indentation 0)
+
 (map!
  :leader
  (:prefix "m"
