65064c9e20dc92a15ecf91e14040b44764f70999
Colin Jaffe <balloonasaurus@gmail.com>
2023-05-02 18:13:53 -0700

    Updates directory locations.

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,6 +1,6 @@
-(("default" . ((user-emacs-directory . "~/.doom-emacs.d")
+(("default" . ((user-emacs-directory . "~/doom-emacs")
                (server-name . "doom")
-               (env . (("DOOMDIR" . "~/.doom-emacs.d")))))
+               (env . (("DOOMDIR" . "~/.doom.d")))))
  ("crafted" . ((user-emacs-directory . "~/.crafted-emacs.d")
                (server-name . "crafted")
                (env . (("CRAFTED_EMACS_HOME" . "~/.config/crafted-emacs"))))))
